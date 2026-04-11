cb333d0c16ae5da77253830031e6296f16c04caf
ChweeBee <leonldobson@gmail.com>
2026-04-11 19:38:48 +0100

    BFS and Grid Movement System
    Created a grid movement system for the board. The characters will later be classed as units to allow them to interact with this. The board will either be a plane on top, or I will figure out what to do.

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,94 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class UnitController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 1f;
+
+    Transform selectedUnit;
+    bool unitSelected = false;
+
+    List<Node> path = new List<Node>();
+
+    GridManager gridManager;
+    Pathfinding pathFinder;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gridManager = FindAnyObjectByType<GridManager>();
+        pathFinder = FindAnyObjectByType<Pathfinding>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            bool hasHit = Physics.Raycast(ray, out hit);
+
+            if (hasHit)
+            {
+                if (hit.transform.tag == "Tile")
+                {
+                    if (unitSelected)
+                    {
+                        Vector2Int targetCords = hit.transform.GetComponent<Tile>().cords;
+                        Vector2Int startCords = new Vector2Int((int) selectedUnit.transform.position.x, (int) selectedUnit.transform.position.z) / gridManager.UnityGridSize;
+                        pathFinder.SetNewDestination(startCords, targetCords);
+                        RecalculatePath(true);
+                    }
+                }
+
+                if (hit.transform.tag == "Unit")
+                {
+                    selectedUnit = hit.transform;
+                    unitSelected = true;
+                }
+            }
+        }
+    }
+
+    void RecalculatePath(bool resetPath)
+    {
+        Vector2Int coordinates = new Vector2Int();
+        
+        if (resetPath)
+        {
+            coordinates = pathFinder.StartCords;
+        }
+        else
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+        }
+
+        StopAllCoroutines();
+        path.Clear();
+        path = pathFinder.GetNewPath(coordinates);
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        for (int i = 1; i < path.Count; i++)
+        {
+            Vector3 startPosition = selectedUnit.position;
+            Vector3 endPosition = gridManager.GetPositionFromCoordinates(path[i].cords);
+            float travelPercent = 0f;
+
+            selectedUnit.LookAt(endPosition);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * movementSpeed;
+                selectedUnit.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
